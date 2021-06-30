bf115206a80d523bfa41164ad343b463f3ccf8b8
Cedric Guillemet <ceguille@microsoft.com>
2021-06-30 19:46:23 +0200

    wfc

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,7 @@ struct Parameters
 {
     uint16_t tileWidth, tileHeight;
     std::string filename;
+    uint16_t mapWidth, mapHeight;
 };
 
 struct Coord
@@ -28,12 +29,12 @@ struct Coord
 int GetValidDirs(Coord coord, Coord *dest, int* directions, uint32_t tileXCount, uint32_t tileYCount)
 {
     int coordCount = 0;
-    if (coord.x < (tileXCount-1))
+    if (coord.x < int32_t(tileXCount-1))
     {
         directions[coordCount] = 0;
         dest[coordCount++] = {1, 0 };
     }
-    if (coord.y < (tileYCount - 1))
+    if (coord.y < int32_t(tileYCount - 1))
     {
         directions[coordCount] = 1;
         dest[coordCount++] = { 0, 1 };
@@ -158,8 +159,13 @@ bool ParseParameters(int argc, char** argv, Parameters& parameters)
 {
     parameters.tileWidth = 16;
     parameters.tileHeight = 16;
+
+    parameters.mapWidth = 4;
+    parameters.mapHeight = 4;
+
 #ifdef _MSC_VER
-    parameters.filename = "../maps/Zelda3LightOverworldBG_masked.png";
+    //parameters.filename = "../maps/Zelda3LightOverworldBG_masked.png";
+    parameters.filename = "../maps/Zelda3LightOverworldBG.png";
 #else
     char cwd[PATH_MAX];
     if (getcwd(cwd, sizeof(cwd)) != NULL)
@@ -169,6 +175,145 @@ bool ParseParameters(int argc, char** argv, Parameters& parameters)
     parameters.filename = std::string(cwd) + "/../../maps/Zelda3LightOverworldBG_masked.png";
 #endif
     return true;
+}
+
+int mWidth, mHeight;
+std::vector<bool> mCoef;
+std::vector<unsigned short> mSumCoef;
+unsigned int mTotalSum;
+int tileCount;
+
+Coord GetMinEntropy()
+{
+    int minEntropy = INT_MAX;
+    static std::vector<Coord> minEntropyCoords;
+    minEntropyCoords.clear();
+    for (int y = 0; y < mHeight; y++)
+    {
+        for (int x = 0; x < mWidth; x++)
+        {
+            int coef = mSumCoef[y * mWidth + x];
+            if (coef == 1)
+                continue;
+            if (coef < minEntropy)
+            {
+                minEntropy = coef;
+                minEntropyCoords.clear();
+            }
+            if (coef == minEntropy)
+                minEntropyCoords.push_back({ x, y });
+        }
+    }
+    assert(!minEntropyCoords.empty());
+    return minEntropyCoords[rand() % minEntropyCoords.size()];
+}
+void Collapse(Coord coord, int tileIndex)
+{
+    int idx = (coord.y * mWidth + coord.x) * tileCount;
+    for (int i = 0; i < tileCount; i++)
+    {
+        if (mCoef[idx + i])
+            mTotalSum--;
+        mCoef[idx + i] = 0;
+    }
+    mCoef[idx + tileIndex] = 1;
+    mTotalSum++;
+    mSumCoef[coord.y * mWidth + coord.x] = 1;
+}
+void Collapse(Coord coord)
+{
+    int *potentials = new int [tileCount];
+    int potentialIndex = 0;
+    int idx = (coord.y * mWidth + coord.x) * tileCount;
+    //auto & v = mCoef[coord.y * mWidth + coord.x];
+    int cnt = 0;
+    for (int i = 0; i < tileCount; i++)
+    {
+        if (mCoef[idx + i])
+            potentials[potentialIndex++] = i;
+    }
+    assert(potentialIndex);
+    static int rd = 0;
+    int selected = potentials[rand() % potentialIndex];
+    delete [] potentials;
+    Collapse(coord, selected);
+}
+bool IsFullyCollapsed()
+{
+    return mTotalSum == (mWidth * mHeight);
+}
+void Constrain(Coord coord, int tileIndex)
+{
+    int idx = (coord.y * mWidth + coord.x) * tileCount;
+    //auto & v = mCoef[coord.y * mWidth + coord.x];
+    assert(mCoef[idx + tileIndex]);
+    mCoef[idx + tileIndex] = 0;
+    mSumCoef[coord.y * mWidth + coord.x] --;
+    mTotalSum--;
+}
+bool TileCompatible(TileIndex tileIndex1, TileIndex tileIndex2, int direction)
+{
+    //int key1 = mTiles[tileIndex1].mKeys[GetAngle(dir)];
+    //int key2 = mTiles[tileIndex2].mKeys[GetHook(dir)];
+
+    //return (key1 & key2) != 0;
+    bool compatible = tiles[tileIndex1].compatibility[direction][tileIndex2];
+    return compatible;
+}
+
+int GetPossibleTiles(Coord coord, int* possibleTiles)
+{
+    int res = 0;
+    int idx = (coord.y * mWidth + coord.x) * tileCount;
+    //auto & v = mCoef[coord.y * mWidth + coord.x];
+    for (int i = 0; i < tileCount; i++)
+    {
+        if (mCoef[idx + i])
+            possibleTiles[res++] = i;
+    }
+    assert(res);
+    return res;
+}
+
+void Propagate(Coord coord)
+{
+    static std::vector<Coord> coords;
+    coords.clear();
+    coords.push_back(coord);
+    while (coords.size())
+    {
+        Coord currentCoord = coords.back();
+        coords.pop_back();
+
+        std::vector<int> curPossibleTiles(tileCount);
+        int curPossibleTileCount = GetPossibleTiles(currentCoord, curPossibleTiles.data());
+
+        Coord validDirs[4];
+        int directions[4];
+        int validDirCount = GetValidDirs(currentCoord, validDirs, directions, mWidth, mHeight);
+        for (int d = 0; d < validDirCount; d++)
+        {
+            Coord dir = validDirs[d];
+            Coord otherCoord = { currentCoord.x + dir.x, currentCoord.y + dir.y };
+            std::vector<int> otherPossibleTiles(tileCount);
+            int otherPossibleTileCount = GetPossibleTiles(otherCoord, otherPossibleTiles.data());
+            for (int otherTileIndex = 0; otherTileIndex < otherPossibleTileCount; otherTileIndex++)
+            {
+                int otherTile = otherPossibleTiles[otherTileIndex];
+                bool tileCompatible = false;
+                for (int curTileIndex = 0; curTileIndex < curPossibleTileCount; curTileIndex++)
+                {
+                    int curTile = curPossibleTiles[curTileIndex];
+                    tileCompatible |= TileCompatible(curTile, otherTile, directions[d]);
+                }
+                if (!tileCompatible)
+                {
+                    Constrain(otherCoord, otherTile);
+                    coords.push_back(otherCoord);
+                }
+            }
+        }
+    }
 }
 
 int main(int argc, char** argv)
@@ -190,15 +335,16 @@ int main(int argc, char** argv)
     
 
     // build tile map
-    const size_t tileXCount = x / parameters.tileWidth;
-    const size_t tileYCount = y / parameters.tileHeight;
+    const uint32_t tileXCount = x / parameters.tileWidth;
+    const uint32_t tileYCount = y / parameters.tileHeight;
     tileMap.resize(tileXCount * tileYCount);
 
-    for (size_t ty = 0; ty < tileYCount; ty++)
+    for (uint32_t ty = 0; ty < tileYCount; ty++)
     {
-        for (size_t tx = 0; tx < tileYCount; tx++)
+        for (uint32_t tx = 0; tx < tileYCount; tx++)
         {
-            TileIndex tileIndex = GetTileIndex((Color*)data, (ty * parameters.tileHeight) * x + tx * parameters.tileWidth, x, parameters);
+            uint32_t offset = (ty * parameters.tileHeight) * x + tx * parameters.tileWidth;
+            TileIndex tileIndex = GetTileIndex((Color*)data, offset, x, parameters);
             tileMap[ty * tileXCount + tx] = tileIndex;
         }
     }
@@ -260,6 +406,27 @@ int main(int argc, char** argv)
         }
     }
     printf("Done\n");
+
+    // do it
+
+    mWidth = parameters.mapWidth;
+    mHeight = parameters.mapHeight;
+    tileCount = int(tiles.size());
+    mCoef.resize(mWidth * mHeight * tileCount, true);
+    mSumCoef.resize(mWidth * mHeight, tileCount);
+    mTotalSum = mWidth * mHeight * tileCount;
+
+    srand(18);
+
+    while (!IsFullyCollapsed())
+    {
+        Coord coord = GetMinEntropy();
+        Collapse(coord);
+        Propagate(coord);
+    }
+
+    // check image
+
 
     stbi_image_free(data);
     return 0;
